75846b5172b49f0338413da1c0c0ea36d93265bb
Morita Eiji <chama1007@icloud.com>
2022-03-08 01:00:57 +0900

    docker

---
+++ scripts/run_docker.sh
@@ -0,0 +1,2 @@
+sudo docker run  -it  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /Users/moritaeiji/robotech/opencv/Learning-OpenCV-3_examples:/root/project  docker-opencv
+#--device /dev/video0
